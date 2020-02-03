2f39440e1ef4568fd45b7cc7d60789379a622012
paulms1 <stevenson.paulmichael@gmail.com>
2020-02-03 15:18:45 +0000

    html basic layout

---
+++ FE/index.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="normalize.css">
+        <link rel="stylesheet" type="text/css" href="styles.css">
+        <title>Record Collection</title>
+    </head>
+    <body>
+        <header>
+            <img>
+        </header>
+    </body>
+</html>
+
+
