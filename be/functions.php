4bbda57ebc4382bc219b11c446872445e15561c3
paulms1 <stevenson.paulmichael@gmail.com>
2020-02-05 15:28:33 +0000

    functions + tests(success + failure)

--- be/functions.php
+++ be/functions.php
@@ -12,10 +12,17 @@ function getAllRecords($db)
 
 function displayRecords(array $records): string
 {
-    $result='';
-    foreach ($records as $record)
-    {
-      $result.= "<div class='surround'>
+        $result = '';
+
+        foreach ($records as $record) {
+
+            if($record['year'] < 2021 && $record['year'] > 1929) {
+                $year = $record['year'];
+            } else {
+                $year = 'N/A';
+            }
+
+            $result .= "<div class='surround'>
             <div class='floater' id='band'>
                 <img src='" . $record['image'] . "'>
             </div>
@@ -25,13 +32,40 @@ function displayRecords(array $records): string
                     <ul>
                         <li>Artist: " . $record['band'] . "</li>
                         <li>Song title: " . $record['song'] . "</li>
-                        <li>Year of release: " . $record['year'] . "</li>
+                        <li>Year of release: " . $year . "</li>
                         <li>Genre: " . $record['genre'] . "</li>
                         <li><a href='" . $record['link'] . "'>Listen on Youtube</a></li>
                     </ul>
                 </div>
             </div>
         </div>";
-    };
-    return $result;
+        };
+
+        return $result;
+
+//    if ($record['year'] < 2021) {
+//        return $result;
+//    } else {
+//        $result = '';
+//        foreach ($records as $record) {
+//            $result .= "<div class='surround'>
+//            <div class='floater' id='band'>
+//                <img src='" . $record['image'] . "'>
+//            </div>
+//
+//            <div class='floater' id='info'>
+//                <div class='list'>
+//                    <ul>
+//                        <li>Artist: " . $record['band'] . "</li>
+//                        <li>Song title: " . $record['song'] . "</li>
+//                        <li>Year of release: N/A</li>
+//                        <li>Genre: " . $record['genre'] . "</li>
+//                        <li><a href='" . $record['link'] . "'>Listen on Youtube</a></li>
+//                    </ul>
+//                </div>
+//            </div>
+//        </div>";
+//        };
+
 };
+
--- tests/functions.php
+++ tests/functions.php
@@ -10,7 +10,7 @@ class FunctionsTests extends TestCase
     {
         $expected = "<div class='surround'>
             <div class='floater' id='band'>
-                <img src='../tests/joyDivision.jpg'>
+                <img src='joyDivision.jpg'>
             </div>
 
             <div class='floater' id='info'>
@@ -20,22 +20,60 @@ class FunctionsTests extends TestCase
                         <li>Song title: Disorder</li>
                         <li>Year of release: 1979</li>
                         <li>Genre: Post Punk</li>
-                        <li><a href='../tests/joyDivision.jpg'>Listen on Youtube</a></li>
+                        <li><a href='https://youtu.be/Q4liMrj1_m8'>Listen on Youtube</a></li>
                     </ul>
                 </div>
             </div>
         </div>";
 
         $input = [
+            [
             'band' => 'Joy Division',
             'song' => 'Disorder',
             'year' => '1979',
             'genre' => 'Post Punk',
             'link' => 'https://youtu.be/Q4liMrj1_m8',
-            'image' => '../tests/joyDivision.jpg'
+            'image' => 'joyDivision.jpg'
+            ]
         ];
-
-        
+        $case = displayRecords($input);
+        $this->assertEquals($expected, $case);
     }
+
+    public function testFailureDisplayRecords()
+    {
+        $expected = "<div class='surround'>
+            <div class='floater' id='band'>
+                <img src='joyDivision.jpg'>
+            </div>
+
+            <div class='floater' id='info'>
+                <div class='list'>
+                    <ul>
+                        <li>Artist: Joy Division</li>
+                        <li>Song title: Disorder</li>
+                        <li>Year of release: N/A</li>
+                        <li>Genre: Post Punk</li>
+                        <li><a href='https://youtu.be/Q4liMrj1_m8'>Listen on Youtube</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>";
+
+        $input = [
+            [
+                'band' => 'Joy Division',
+                'song' => 'Disorder',
+                'year' => '1915',
+                'genre' => 'Post Punk',
+                'link' => 'https://youtu.be/Q4liMrj1_m8',
+                'image' => 'joyDivision.jpg'
+            ]
+        ];
+        $case = displayRecords($input);
+        $this->assertEquals($expected, $case);
+    }
+
 }
 
+    
